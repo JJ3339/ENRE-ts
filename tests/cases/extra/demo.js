cae3ba88bc0b46ec44d026945ed4638b8382892e
JJ3339 <2530629811@qq.com>
2024-09-24 10:42:55 +0800

    test

---
+++ tests/cases/extra/demo.js
@@ -0,0 +1,25 @@
+import {Column,Text,Setheight,setdeep,seta} from '@ArkTS';
+
+/*@Component
+struct Foo{
+    build(){
+      Column(){
+        Text('ArkTS')
+        .Setheight(100)
+        .setdeep(100)
+      }.seta(1)
+    }
+}*/
+function(){
+
+}
+class foo{
+    constructor(){
+       Column(
+        Text('ArkTS'
+        ,Setheight(100)
+        ,setdeep(100))
+      ).seta(1);
+    }
+}
+
