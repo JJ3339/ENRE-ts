3999897e090d58c12448e6b72e81cc266a2c31bb
JJ3339 <2530629811@qq.com>
2024-10-22 14:06:49 +0800

    stop tracking `out` folder

--- tests/cases/extra/demo.js
+++ tests/cases/extra/demo.js
@@ -25,15 +25,15 @@ class Simple {
 new Simple()
   .add()        // 10 + 5 = 15
   .subtract()   // 15 - 3 = 12
-var Obj = {
-  result: 0,
-  addNumber: function (a, b) {
-      this.result = a + b;
-      return this;
-  },
-  multiplyNumber: function (a) {
-      this.result = this.result * a;
-      return this;
-  },
-}
-Obj.addNumber(10, 20).multiplyNumber(10);
+// var Obj = {
+//   result: 0,
+//   addNumber: function (a, b) {
+//       this.result = a + b;
+//       return this;
+//   },
+//   multiplyNumber: function (a) {
+//       this.result = this.result * a;
+//       return this;
+//   },
+// }
+// Obj.addNumber(10, 20).multiplyNumber(10);
